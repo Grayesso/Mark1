879cd9d518e2b9595d8c256105729509482c274c
Gray <94595794+Grayesso@users.noreply.github.com>
2025-08-17 00:02:47 +0300

    Mark1.cpp

--- Mark1.cpp
+++ Mark1.cpp
@@ -598,8 +598,7 @@ int main(int argc,char** argv)
 
     bloom=new simd_bloom::SimdBlockFilterFixed<>(bloomBytes);
 
-    unsigned tt=std::max(1u,std::thread::hardware_concurrency());
-    unsigned th-4;
+    unsigned th=std::max(1u,std::thread::hardware_concurrency());
     auto segs=splitRange(A,range,th);
     uint64_t per=(traps+th-1)/th;
     buildJumpTable(k);
@@ -681,4 +680,3 @@ int main(int argc,char** argv)
     delete bloom; dp.close();
     return 0;
 }
-
